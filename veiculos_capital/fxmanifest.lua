dc9f48a16d526a9e0cf7f1ce8d24f1288c11f377
bluenzzz <grupocapitaldevs@gmail.com>
2025-02-12 20:55:11 -0300

    add vehicles 2

---
+++ veiculos_capital/fxmanifest.lua
@@ -0,0 +1,47 @@
+shared_script "@vrp/lib/lib.lua" 
+
+
+fx_version "bodacious"
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"data/**/*.meta",
+	"audio/*",
+	"audio/**/*"
+}
+
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "data/**/popgroups.ymt"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r35sound/dlc_r35sound"
+data_file "AUDIO_GAMEDATA" "audio/r35sound/r35sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r35sound/r35sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/bnr34sound/dlc_bnr34ffeng"
+data_file "AUDIO_GAMEDATA" "audio/bnr34sound/bnr34ffeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/bnr34sound/bnr34ffeng_sounds.dat"
+data_file "AUDIO_SYNTHDATA" "audio/bnr34sound/bnr34ffeng_amp.dat"
+ 
+data_file "AUDIO_WAVEPACK" "audio/lfasound/dlc_lfasound"
+data_file "AUDIO_GAMEDATA" "audio/lfasound/lfasound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/lfasound/lfasound_sounds.dat"
+              
+
+              
+
+              
+
