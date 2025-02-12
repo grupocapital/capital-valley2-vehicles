c668bb1e002d43f7ee6c07d7a63a9f8d722ca94f
bluenzzz <grupocapitaldevs@gmail.com>
2025-02-12 20:46:12 -0300

    add vehicles 1

---
+++ viaturas_capital/fxmanifest.lua
@@ -0,0 +1,24 @@
+-- shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+
+fx_version "bodacious"
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"data/**/*.meta",
+	"audio/*",
+	"audio/**/*"
+}
+
+-- METAS
+data_file "HANDLING_FILE" "data/**/*.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/*.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/*.meta"
+data_file "CARCOLS_FILE" "data/**/*.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/*.meta"
+              
+
+              
+
