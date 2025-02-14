4215c816079c606149487920f0b050175660d85a
bluenzzz <grupocapitaldevs@gmail.com>
2025-02-14 17:16:38 -0300

    sync

--- viaturas_capital/fxmanifest.lua
+++ viaturas_capital/fxmanifest.lua
@@ -1,5 +1,4 @@
--- shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
-
+shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
 
 fx_version "bodacious"
 game "gta5"
